80ab5492518b072e06778999eb11d65ad06621a1
Brian Thornburg <briant3275@gmail.com>
2022-02-22 21:26:51 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+'use strict';
+
+console.log('Babys first server!');
+
+const express = require('express');
+const req = require('express/lib/request');
+
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 3002;
+
+const weatherData = require('./data/weather.json');
+
+app.get('/', (request, response) => {
+  response.send('Howdy from the Server!');
+});
+
+app.get('/banana', (request, response) => {
+  response.send('Nanners');
+});
+
+app.get('/sayHello', (request, response) => {
+  let name = request.query.name;
+  console.log(request.query);
+  console.log(name);
+  response.status(200).send(`Hello ${name}, from the server!`);
+});
+
+app.get('/throw-an-error', (request, response) => {
+  throw 'You done effed up';
+});
+
+app.get('/weather', (request, response) => {
+  let cityName = request.query.city_name;
+  console.log(cityName);
+  response.send(weatherData.filter(weather => weather.cityName === cityName).map(weather => new Weather(weather)));
+});
+
+app.get('*', (request, response) => {
+  response.status(404).send(`this ain't it bruh`);
+});
+
+class Weather {
+  constructor(weather){
+    this.cityName = weather.city_name;
+    this.temp = weather.temp;
+  }
+}
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
