2a3653afa0c3a0948df9f77d0648cd6d7b4a730a
Brian Thornburg <briant3275@gmail.com>
2022-03-05 22:01:52 -0600

    trying to fix movie display

--- movies.js
+++ movies.js
@@ -8,11 +8,11 @@ async function getMovies (request, response) {
     let nameOfCity = request.query.nameOfCity;
     let key = 'movies-' + nameOfCity;
     if(cache[key] && (Date.now() - cache[key].timestamp < 1000 * 60 * 60 * 24)){
-      console.log('Cache Hit, Movies data present');
+      console.log('Cache Hit, Movies present');
       response.status(200).send(cache[key].data);
     } else {
-      console.log('Cache Miss, Movies data not present');
-      let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_API_KEY}&query=${nameOfCity}`;
+      console.log('Cache Miss, Movies not present');
+      let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${nameOfCity}`;
 
       let movieResults = await axios.get(movieUrl);
 
@@ -22,6 +22,10 @@ async function getMovies (request, response) {
       cache[key]={};
       cache[key].timestamp = Date.now();
       cache[key].data = movieArr;
+
+      console.log(movieResults);
+      console.log(movieArr);
+
       response.send(movieArr);
     }
     return cache[key].data;
@@ -33,10 +37,11 @@ async function getMovies (request, response) {
 class Movies {
   constructor(movie) {
     this.title = movie.original_title;
-    this.release = movie.release_date;
     this.overview = movie.overview;
     this.rating = movie.vote_average;
     this.votes = movie.vote_count;
+    this.release = movie.release_date;
+    this.img_url = movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : 'no-poster';
   }
 }
 
